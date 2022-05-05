b360f294fe5f4f6128511c6becc4fe54a615ae67
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-05 16:49:39 +0200

    set up project with core data

---
+++ DadJokes/DadJokes/EmojiView.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiView.swift
+//  DadJokes
+//
+//  Created by Aleksandar Filipov on 5/5/22.
+//
+
+import SwiftUI
+
+struct EmojiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiView()
+    }
+}
---
+++ DadJokes/DadJokes/Joke+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Joke+CoreDataClass.swift
+//  DadJokes
+//
+//  Created by Aleksandar Filipov on 5/5/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Joke)
+public class Joke: NSManagedObject {
+
+}
