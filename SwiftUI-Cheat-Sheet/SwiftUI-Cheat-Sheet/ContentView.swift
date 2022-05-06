294adde50408a714bf1411b8a73d3e46fd70c49d
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-06 15:44:07 +0200

    iOS project with Mac Catalist

---
+++ SwiftUI-Cheat-Sheet/SwiftUI-Cheat-Sheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Cheat-Sheet
+//
+//  Created by Aleksandar Filipov on 5/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Cheat-Sheet/SwiftUI-Cheat-Sheet/SwiftUI_Cheat_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Cheat_SheetApp.swift
+//  SwiftUI-Cheat-Sheet
+//
+//  Created by Aleksandar Filipov on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Cheat_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
