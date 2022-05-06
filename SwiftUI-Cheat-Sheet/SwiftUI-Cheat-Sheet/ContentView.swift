180852ebb03ba7148c78eedbeb9401bbab4c5810
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-06 16:00:36 +0200

    Splitting up large views

--- SwiftUI-Cheat-Sheet/SwiftUI-Cheat-Sheet/ContentView.swift
+++ SwiftUI-Cheat-Sheet/SwiftUI-Cheat-Sheet/ContentView.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let users = ["Paul Hudson", "Taylor Swift"]
+
     var body: some View {
-            Text("Colored ")
-                .foregroundColor(.red)
-            +
-            Text("SwifUI ")
-                .foregroundColor(.green)
-            +
-            Text("Text")
-                .foregroundColor(.blue)
+        NavigationView {
+            List(users, id: \.self) { user in
+                NavigationLink(destination: Text("Detail View")) {
+                    ExtractedView() // extracted with Cmd-click
+
+                    VStack(alignment: .leading) {
+                        Text(user)
+                            .font(.headline)
+                        Text("Occupation: Programmer")
+                    }
+                }
+            }
+            .navigationTitle("Users")
+        }
     }
 }
 
@@ -26,42 +34,10 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-extension View {
-    func iOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
-        #if os(iOS)
-        return modifier(self)
-        #else
-        return self
-        #endif
-    }
-}
-
-extension View {
-    func macOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
-        #if os(macOS)
-        return modifier(self)
-        #else
-        return self
-        #endif
-    }
-}
-
-extension View {
-    func tvOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
-        #if os(tvOS)
-        return modifier(self)
-        #else
-        return self
-        #endif
-    }
-}
-
-extension View {
-    func watchOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
-        #if os(watchOS)
-        return modifier(self)
-        #else
-        return self
-        #endif
+struct ExtractedView: View {
+    var body: some View {
+        Image(systemName: "sun.max")
+            .resizable()
+            .frame(width: 50, height: 50)
     }
 }
