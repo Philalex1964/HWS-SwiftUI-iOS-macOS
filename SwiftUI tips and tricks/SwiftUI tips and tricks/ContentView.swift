42feaed83177d57def64eb1c99a52d7c4e3d798b
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-06 15:20:22 +0200

    Add customizations for an individual platform

--- SwiftUI tips and tricks/SwiftUI tips and tricks/ContentView.swift
+++ SwiftUI tips and tricks/SwiftUI tips and tricks/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
         VStack {
             Text("Hello, world!")
                 .padding()
+                .iOS { $0.padding(10) }
             
             TextField("Example placeholder", text: .constant("Hello"))
                 .textFieldStyle(.roundedBorder)
@@ -26,5 +27,45 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func iOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(iOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+}
+
+extension View {
+    func macOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(macOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+}
+
+extension View {
+    func tvOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(tvOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+}
+
+extension View {
+    func watchOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(watchOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
     }
 }
