8b0a798e366b3ba5a4d76e0298548e3f431dfacc
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-06 15:44:25 +0200

    Combine text views

--- SwiftUI-Cheat-Sheet/SwiftUI-Cheat-Sheet/ContentView.swift
+++ SwiftUI-Cheat-Sheet/SwiftUI-Cheat-Sheet/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            Text("Colored ")
+                .foregroundColor(.red)
+            +
+            Text("SwifUI ")
+                .foregroundColor(.green)
+            +
+            Text("Text")
+                .foregroundColor(.blue)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func iOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(iOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+}
+
+extension View {
+    func macOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(macOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+}
+
+extension View {
+    func tvOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(tvOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+}
+
+extension View {
+    func watchOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(watchOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
     }
 }
