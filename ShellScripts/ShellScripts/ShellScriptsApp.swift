5d235ac155bd508d5117af02767c71d5cb066b68
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-10 09:47:39 +0200

    set project

---
+++ ShellScripts/ShellScripts/ShellScriptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShellScriptsApp.swift
+//  ShellScripts
+//
+//  Created by Aleksandar Filipov on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShellScriptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
